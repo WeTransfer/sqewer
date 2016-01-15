6b9150c64cc68949a44073d54f1ca02f245d1e93
Julik Tarkhanov <me@julik.nl>
2016-01-15 15:03:18 +0100

    Context is a required argument
    for each middleware

--- lib/conveyor_belt/contrib/appsignal_wrapper.rb
+++ lib/conveyor_belt/contrib/appsignal_wrapper.rb
@@ -1,11 +1,9 @@
 module ConveyorBelt
   module Contrib
-    
     # Can be used as a wrapper middleware in an ExecutionContext to log exceptions
     # to Appsignal and to monitor performance. Will only activate
     # if the Appsignal gem is loaded within the current process and active.
     class AppsignalWrapper
-      
       # Unserialize the job
       def around_deserialization(serializer, msg_id, msg_payload)
         return yield unless (defined?(Appsignal) && Appsignal.active?)
@@ -17,15 +15,15 @@ module ConveyorBelt
       end
   
       # Run the job with Appsignal monitoring.
-      def around_execution(job)
+      def around_execution(job, context)
         return yield unless (defined?(Appsignal) && Appsignal.active?)
-    
+        
         Appsignal.monitor_transaction('perform_job.conveyor_belt', 
-          :class => job.class.to_s, :params => job.to_h, :method => 'run') do
+          :class => job.class.to_s, :params => job.to_h, :method => 'run') do |t|
+            context['appsignal.transaction'] = t
           yield
         end
       end
-    
     end
   end
 end

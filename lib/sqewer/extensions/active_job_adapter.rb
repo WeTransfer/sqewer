cd0ded9fa2a4446fa2e360100ccfea83ae6b96c9
Julik Tarkhanov <me@julik.nl>
2016-04-07 17:14:22 +0200

    Make ActiveRecord connection optional
    If no connection is established, do not attempt
    to check out a connection from the pool just for
    executing the job. Let the job fail if it still
    wants to do something with AR but the connection
    is not properly set up in the Rails stack.

--- lib/sqewer/extensions/active_job_adapter.rb
+++ lib/sqewer/extensions/active_job_adapter.rb
@@ -1,3 +1,5 @@
+require 'active_record'
+
 # ActiveJob docs: http://edgeguides.rubyonrails.org/active_job_basics.html
 # Example adapters ref: https://github.com/rails/rails/tree/master/activejob/lib/active_job/queue_adapters
 module ActiveJob
@@ -37,7 +39,11 @@ module ActiveJob
 
         # Runs the contained ActiveJob.
         def run
-          ActiveRecord::Base.connection_pool.with_connection do
+          if ActiveRecord::Base.connected?
+            ActiveRecord::Base.connection_pool.with_connection do
+              Base.execute @job.stringify_keys
+            end
+          else
             Base.execute @job.stringify_keys
           end
         end

0d924449e251ad65deae6441ce0cfdd44d6bafb1
Julik Tarkhanov <me@julik.nl>
2016-04-08 15:59:54 +0200

    Tiny railtie tweak

--- lib/sqewer/extensions/rails.rb
+++ lib/sqewer/extensions/rails.rb
@@ -1,13 +1,3 @@
 module Sqewer
-  def self.load_rails
-    # Adapted from: https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/cli.rb
 
-    require 'rails'
-    # Painful contortions, see https://github.com/mperham/sidekiq/issues/1791
-    require File.expand_path('config/application.rb')
-    ::Rails::Application.initializer 'sqewer.eager_load' do
-      ::Rails.application.config.eager_load = true
-    end
-    require File.expand_path('config/environment.rb')
-  end
 end
--- lib/sqewer/extensions/railtie.rb
+++ lib/sqewer/extensions/railtie.rb
@@ -1,8 +1,22 @@
 module Sqewer
+  require 'sqewer/extensions/active_job_adapter' if defined?(::ActiveJob)
+
+  # Ensures that within the Sqewer worker process, the Rails eager loading of classes
+  # is forcibly enabled (Sqewer is threaded and autoloading does not play nice with that).
+  def self.force_enable_eager_loading_of_rails_classes
+    # Adapted from: https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/cli.rb
+    require 'rails'
+    # Painful contortions, see https://github.com/mperham/sidekiq/issues/1791
+    require File.expand_path('config/application.rb')
+    ::Rails::Application.initializer 'sqewer.eager_load' do
+      ::Rails.application.config.eager_load = true
+    end
+    require File.expand_path('config/environment.rb')
+  end
+
   # Loads the Sqewer components that provide ActiveJob compatibility
   class Railtie < Rails::Railtie
     initializer "sqewer.load_active_job_adapter" do |app|
-      require 'sqewer/extensions/active_job_adapter' if defined?(::ActiveJob)
     end
   end
 end

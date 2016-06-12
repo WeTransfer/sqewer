9f0fc1500d2dc3c4779eea67ee0681eaf99b9a61
Julik Tarkhanov <me@julik.nl>
2016-06-12 18:03:55 +0200

    Add a test with a mock Worker

--- spec/sqewer/cli_spec.rb
+++ spec/sqewer/cli_spec.rb
@@ -4,7 +4,30 @@ describe Sqewer::CLI, :sqs => true, :wait => {timeout: 120} do
   after :each do
     Dir.glob('*-result').each{|path| File.unlink(path) }
   end
-    
+  
+  describe 'with a mock Worker' do
+    it 'uses just three methods' do
+      mock_worker = Class.new do
+        def self.start; end
+        def self.stop; end
+        def self.debug_thread_information!; end
+      end
+      
+      worker_pid = fork do
+        Sqewer::CLI.start(mock_worker)
+      end
+      sleep 1
+      
+      Process.kill('INFO', worker_pid) # Calls debug_thread_information!
+      Process.kill('TERM', worker_pid) # Terminates the worker
+      
+      wait_for { 
+        _, status = Process.wait2(worker_pid)
+        expect(status.exitstatus).to be_zero # Must have quit cleanly
+      }
+    end
+  end
+  
   describe 'runs the commandline app, executes jobs and then quits cleanly' do
     it 'on a USR1 signal' do
       submitter = Sqewer::Connection.default
@@ -19,8 +42,11 @@ describe Sqewer::CLI, :sqs => true, :wait => {timeout: 120} do
       end
 
       sleep 8
-      Process.kill("USR1", pid)
-      wait_for { Process.wait(pid) }
+      wait_for { 
+        Process.kill("USR1", pid)
+        _, status = Process.wait2(pid)
+        expect(status.exitstatus).to be_zero # Must have quit cleanly
+      }
       
       generated_files = Dir.glob('*-result')
       expect(generated_files).not_to be_empty
@@ -40,8 +66,11 @@ describe Sqewer::CLI, :sqs => true, :wait => {timeout: 120} do
       end
 
       sleep 8
-      Process.kill("TERM", pid)
-      wait_for { Process.wait(pid) }
+      wait_for { 
+        Process.kill("TERM", pid)
+        _, status = Process.wait2(pid)
+        expect(status.exitstatus).to be_zero # Must have quit cleanly
+      }
       
       generated_files = Dir.glob('*-result')
       expect(generated_files).not_to be_empty

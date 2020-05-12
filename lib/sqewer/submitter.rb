f765cabef3c293e8c7f19b2c625a4829eec45e76
Julik Tarkhanov <me@julik.nl>
2020-05-12 22:05:34 +0200

    Raise immediately when message is too large
    instead of waiting for the messagebox to be
    flushed into the connection - as this would
    create an error outside of the error tracking middleware,
    preventing recovery and actually failing silently.
    
    When a message turns out to be too large, fail immediately
    and do not forward the mesasge to connection/messagebox.

--- lib/sqewer/submitter.rb
+++ lib/sqewer/submitter.rb
@@ -3,7 +3,10 @@
 # and the serializer (something that responds to `#serialize`) to
 # convert the job into the string that will be put in the queue.
 class Sqewer::Submitter < Struct.new(:connection, :serializer)
+  MAX_PERMITTED_MESSAGE_SIZE_BYTES = 256 * 1024
+
   NotSqewerJob = Class.new(Sqewer::Error)
+  MessageTooLarge = Class.new(Sqewer::Error)
 
   # Returns a default Submitter, configured with the default connection
   # and the default serializer.
@@ -12,7 +15,7 @@ class Sqewer::Submitter < Struct.new(:connection, :serializer)
   end
 
   def submit!(job, **kwargs_for_send)
-    raise NotSqewerJob.new("Submitted object is not a valid job: #{job.inspect}") unless job.respond_to?(:run)
+    validate_job_responds_to_run!(job)
     message_body = if delay_by_seconds = kwargs_for_send[:delay_seconds]
       clamped_delay = clamp_delay(delay_by_seconds)
       kwargs_for_send[:delay_seconds] = clamped_delay
@@ -21,10 +24,25 @@ class Sqewer::Submitter < Struct.new(:connection, :serializer)
     else
       serializer.serialize(job)
     end
+    validate_message_for_size!(message_body, job)
+
     connection.send_message(message_body, **kwargs_for_send)
   end
 
   private
+
+  def validate_job_responds_to_run!(job)
+    return if job.respond_to?(:run)
+    error_message = "Submitted object is not a valid job (does not respond to #run): #{job.inspect}"
+    raise NotSqewerJob.new(error_message)
+  end
+
+  def validate_message_for_size!(message_body, job)
+    actual_bytesize = message_body.bytesize
+    return if actual_bytesize <= MAX_PERMITTED_MESSAGE_SIZE_BYTES
+    error_message = "Job #{job.class.inspect} serialized to a message which was too large (#{actual_bytesize} bytes)"
+    raise MessageTooLarge.new(error_message)
+  end
 
   def clamp_delay(delay)
     [1, 899, delay].sort[1]
--- spec/sqewer/submitter_spec.rb
+++ spec/sqewer/submitter_spec.rb
@@ -4,13 +4,13 @@ describe Sqewer::Submitter do
   describe '.default' do
     it 'returns a set up Submitter with the configured Connection and Serializer' do
       expect(ENV).to receive(:fetch).with('SQS_QUEUE_URL').and_return('https://some-queue.aws.com')
-      
+
       s = described_class.default
       expect(s.connection).to respond_to(:send_message)
       expect(s.serializer).to respond_to(:serialize)
     end
   end
-  
+
   describe '#initialize' do
     it 'creates a Submitter that you can submit jobs through' do
       fake_serializer = double('Some serializer')
@@ -18,61 +18,79 @@ describe Sqewer::Submitter do
         expect(object_to_serialize).not_to be_nil
         'serialized-object-data'
       }
-      
+
       fake_connection = double('Some SQS connection')
       expect(fake_connection).to receive(:send_message).at_least(5).times.with('serialized-object-data', any_args)
 
       fake_job = double('Some job', run: true)
-      
+
       subject = described_class.new(fake_connection, fake_serializer)
       5.times { subject.submit!(fake_job) }
     end
-    
+
     it 'passes the keyword arguments to send_message on the connection' do
       fake_serializer = double('Some serializer')
       allow(fake_serializer).to receive(:serialize) {|object_to_serialize|
         expect(object_to_serialize).not_to be_nil
         'serialized-object-data'
       }
-      
+
       fake_connection = double('Some SQS connection')
       expect(fake_connection).to receive(:send_message).with('serialized-object-data', {delay_seconds: 5})
 
       fake_job = double('Some job', run: true)
-      
+
       subject = described_class.new(fake_connection, fake_serializer)
       subject.submit!(fake_job, delay_seconds: 5)
     end
-    
+
     it 'handles the massively delayed execution by clamping the delay_seconds to the SQS maximum, and saving the _execute_after' do
       fake_serializer = double('Some serializer')
       allow(fake_serializer).to receive(:serialize) {|object_to_serialize, timestamp_seconds|
-        
+
         delay_by = Time.now.to_i + 4585659855
         expect(timestamp_seconds).to be_within(20).of(delay_by)
-        
+
         expect(object_to_serialize).not_to be_nil
         'serialized-object-data'
       }
-      
+
       fake_connection = double('Some SQS connection')
       expect(fake_connection).to receive(:send_message).with('serialized-object-data', {delay_seconds: 899})
 
       fake_job = double('Some job', run: true)
-      
+
       subject = described_class.new(fake_connection, fake_serializer)
       subject.submit!(fake_job, delay_seconds: 4585659855)
     end
 
-    it "raises an error if the job does not respond to call" do
+    it "raises an error if the job does not respond to #run" do
       fake_serializer = double('Some serializer')
       fake_connection = double('Some SQS connection')
       fake_job = double('Some job')
-      
+
       subject = described_class.new(fake_connection, fake_serializer)
       expect {
         subject.submit!(fake_job, delay_seconds: 5)
       }.to raise_error(Sqewer::Submitter::NotSqewerJob)
+    end
+
+    it "raises an error if the job produces a message above the SQS size limit" do
+      class VeryLargeTestJob < Struct.new(:some_data)
+        def run
+        end
+      end
+
+      large_blob = Base64.strict_encode64(Random.new.bytes(257*1024))
+      large_job = VeryLargeTestJob.new(large_blob)
+
+      fake_serializer = Sqewer::Serializer.default
+      fake_connection = double('Some SQS connection')
+
+      subject = described_class.new(fake_connection, fake_serializer)
+      expect {
+        subject.submit!(large_job)
+      }.to raise_error(Sqewer::Submitter::MessageTooLarge, /VeryLargeTestJob/)
     end
   end
 end
